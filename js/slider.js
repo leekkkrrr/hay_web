e993a400750a4d589a794e7bbe4023988d9d1e26
leekkkrrr <136429939+leekkkrrr@users.noreply.github.com>
2023-08-04 16:09:57 +0900

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -14,6 +14,7 @@ var swiper = new Swiper(".newSwiper", {
 var swiper = new Swiper(".productSwiper", {
   slidesPerView: "auto",
   freeMode: true,
+  slideToClickedSlide:true,
   scrollbar: {
     el: ".swiper-scrollbar-horizontal",
     draggable: true,
