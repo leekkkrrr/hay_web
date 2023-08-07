fbc5486fe4a618d24024e7dfe9dbdd6e86de2604
leekkkrrr <136429939+leekkkrrr@users.noreply.github.com>
2023-08-07 16:54:05 +0900

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -22,5 +22,3 @@ var swiper = new Swiper(".productSwiper", {
   mousewheel: true,
 });
 
-
-
--- js/style.js
+++ js/style.js
@@ -1,18 +1,36 @@
 // scroll
-$(window).scroll(function () {
-  let Cscroll = $(this).scrollTop();
+// $(window).scroll(function () {
+//   let Cscroll = $(this).scrollTop();
+//   $(".scroll").text(Cscroll);
+
+//   if (Cscroll >= $("section:nth-child(1)").offset().top - 120) {
+//     $(".best_rank ").addClass("show").siblings().removeClass("show");
+//   }
+// });
+
+function scrollHandler() {
+  let Cscroll = $(window).scrollTop();
   $(".scroll").text(Cscroll);
 
-  if (Cscroll >= $("section:nth-child(1)").offset().top - 120) {
-    $(".best_rank ").addClass("show").siblings().removeClass("show");
+  if (window.matchMedia("(max-width: 420px)").matches) {
+    if (Cscroll >= $(".best").offset().top - 80) {
+      $(".best_rank").addClass("show").siblings().removeClass("show");
+    }
+  } else {
+    if (Cscroll >= $(".best").offset().top - 120) {
+      $(".best_rank").addClass("show").siblings().removeClass("show");
+    }
   }
-});
 
-$(window).scroll(function () {
-  let Cscroll = $(this).scrollTop();
-  $(".scroll").text(Cscroll);
-
-  if (Cscroll >= $("section:nth-child(2)").offset().top - 150) {
-    $(".product>div").addClass("show").siblings().removeClass("show");
+  if (window.matchMedia("(max-width: 420px)").matches) {
+    if (Cscroll >= $("section:nth-child(2)").offset().top - 130) {
+      $(".product>div").addClass("show").siblings().removeClass("show");
+    }
+  } else {
+    if (Cscroll >= $("section:nth-child(2)").offset().top - 150) {
+      $(".product>div").addClass("show").siblings().removeClass("show");
+    }
   }
-});
+}
+
+$(window).scroll(scrollHandler);
